2d0687fd2db045a5fb33621293ff8b72aa0c3c30
klongley <klongley@bannerengineering.com>
2018-07-16 18:04:28 -0500

    Finished adding class

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import year
+
 print("Hello World")
+age = year.Year(17)
+
+print(age.startingYear)
+print(age.getCurrentYear())
+age.incrementYear()
+print(age.getCurrentYear())
+print(age.getAbsoluteYear())
